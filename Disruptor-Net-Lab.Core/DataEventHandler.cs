07700b42b9006e56db8d21f70e05d8b7dc540067
blackie.tsai <blackie1019@gmail.com>
2018-08-10 17:37:42 +0800

    Added

---
+++ Disruptor-Net-Lab.Core/DataEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using Disruptor;
+
+namespace Disruptor_Net_Lab.App
+{
+    public class DataEventHandler:IEventHandler<DataVO>
+    {
+        public string Name;
+
+        public DataEventHandler(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void OnEvent(DataVO data, long sequence, bool endOfBatch)
+        {
+            Console.WriteLine("Thread = {0}, Handler = {1}, Sequence = {2}, Value = {3}", Thread.CurrentThread.ManagedThreadId.ToString(), this.Name, sequence.ToString(), data.Value); 
+
+        }
+    }
+}
---
+++ Disruptor-Net-Lab.Core/DataVO.cs
@@ -0,0 +1,8 @@
+﻿namespace Disruptor_Net_Lab.App
+{
+    public class DataVO
+    {
+        public string Value;
+        public string Key;
+    }
+}
